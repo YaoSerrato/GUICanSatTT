73d9e3171840cd203a979ed20dc6f533ae43f431
H369169 <rodrigoyaoctzin.serratoandrade@honeywell.com>
2019-10-31 17:04:00 -0600

    Finished the standalone usage of the clsDisplay class. This file is just an example; not used in the main file.

--- src/Display_setup.py
+++ src/Display_setup.py
@@ -1,3 +1,24 @@
-class clsDisplay:
-    def __init__():
-        
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QStyleFactory, QApplication
+
+from Display_Window_GUIcode import *
+
+class clsDisplay(QMainWindow):
+    def __init__(self):
+        # Calling parent constructor
+        super().__init__()
+
+        # Creating graphical environment
+        self.uiclsDisplay = Ui_DisplayPlots_Window()
+        self.uiclsDisplay.setupUi(self)
+        self.show()
+
+if __name__=="__main__":
+    app_display = QApplication(sys.argv)
+    app_display.setStyle(QStyleFactory.create('Fusion'))    # ['windowsvista', 'Windows', 'Fusion']
+
+    objclsDisplay_Plotter = clsDisplay()
+    
+    sys.exit(app_display.exec_())
